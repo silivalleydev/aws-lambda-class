aebecc0df9cb977b7a2338989c81c6c8029ebbd8
silivalleydev <silivalleydev@gmail.com>
2021-07-17 11:53:47 +0900

    data

--- api-test/index.js
+++ api-test/index.js
@@ -1,8 +1,5 @@
-const AWS = require('aws-sdk');
 const connectInfo = require('./env');
 const mysql = require('mysql');
-var async = require('async');
-var BreakException = {};
 
 const conn = mysql.createConnection({
     host: connectInfo.host,
@@ -12,29 +9,36 @@ const conn = mysql.createConnection({
     database: connectInfo.database
 })
 exports.handler = async function(event, context, callback){
-    let body;
+    let data;
     let statusCode = '200';
     const headers = {
         'Content-Type': 'application/json',
     };
-    const dbResult = await new Promise((resolve, reject) => {
-        conn.query(
-            'SELECT * FROM board',
-            function(err, rows, fields) {
-                console.log('검색결과?', err, rows, fields);
-                if (!err) {
-                    resolve(rows);
-                } else {
-                    console.log('Error while performing Query.', err);
-                    reject(err)
+    try {
+        data = await new Promise((resolve, reject) => {
+            conn.query(
+                'SELECT * FROM board',
+                function(err, rows, fields) {
+                    console.log('검색결과?', err, rows, fields);
+                    if (!err) {
+                        resolve(rows);
+                    } else {
+                        console.log('Error while performing Query.', err);
+                        reject(err)
+                    }
                 }
-            }
-        );
-    })
+            );
+        });
+    } catch (error) {
+        statusCode = '400';
+    }
 
     return {
-        statusCode: '200',
-        body: JSON.stringify(dbResult),
+        statusCode,
+        body: {
+            status: statusCode,
+            data: JSON.stringify(dbResult)
+        },
         headers,
     };
 
